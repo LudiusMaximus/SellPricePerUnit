56d69fb2b7e90176d65d1bb39c12ff341732cd1f
LudiusMaximus <ludiusmaximus@gmail.com>
2019-08-02 21:17:42 +0200

    Added "if link then" just to be sure...

--- main.lua
+++ main.lua
@@ -35,12 +35,14 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
   if not self.shownMoneyFrames then return end
 
   local name, link = self:GetItem()
-    
+
+  -- Just to be on the safe side...
+  if not name or not link then return end
+
   local _, _, _, _, _, _, _, itemStackCount, _, _, itemSellPrice = GetItemInfo(link)
-  
-  
+
   if itemStackCount == nil or itemStackCount == 1 or itemSellPrice == 0 then return end
-  
+
   -- Get the number of items in stack.
   -- Inspired by: https://www.wowinterface.com/downloads/info25078-BetterVendorPrice.html
   local focusFrame = GetMouseFocus()
@@ -48,12 +50,12 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
     stackCount = focusFrame.count
   end
   if not stackCount or stackCount <= 1 then return end
-  
+
 
   -- The money frame is anchored to a blank line of the tootlip.
   -- Find out which line it is.
   local moneyFrameLineNumber = nil
-  
+
   -- Check all shown money frames of the tooltip.
   -- (There is normally only one, except other addons have added more.)
   for i = 1, self.shownMoneyFrames, 1 do
@@ -63,17 +65,17 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
     -- If the money frame's PrefixText is "SELL_PRICE:", we assume it is the one we are looking for.
     if _G[moneyFrameName.."PrefixText"]:GetText() == string_format("%s:", SELL_PRICE) then
       local _, moneyFrameAnchor = _G[moneyFrameName]:GetPoint(1)
-      
+
       -- Get line number.
       moneyFrameLineNumber = tonumber(string_match(moneyFrameAnchor:GetName(), self:GetName().."TextLeft(%d+)"))
-      
+
       -- We could take the total money value of the stack from _G[moneyFrameName].staticMoney
       -- but when Bagnon displays the items of other characters, it only shows the price of a
       -- single item even for stacks. That's why we are calculating itemSellPrice*stackCount.
 
       break
     end
-    
+
   end
 
 
@@ -87,15 +89,15 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
   local leftTextR = {}
   local leftTextG = {}
   local leftTextB = {}
-  
+
   local rightText = {}
   local rightTextR = {}
   local rightTextG = {}
   local rightTextB = {}
-  
+
   -- Store the number of lines for after ClearLines().
   local numLines = self:NumLines()
-  
+
   -- Store all lines of the original tooltip.
   for i = 1, numLines, 1 do
     leftText[i] = _G[self:GetName().."TextLeft"..i]:GetText()
@@ -104,16 +106,16 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
     rightText[i] = _G[self:GetName().."TextRight"..i]:GetText()
     rightTextR[i], rightTextG[i], rightTextB[i] = _G[self:GetName().."TextRight"..i]:GetTextColor()
   end
-  
-  
+
+
   self:ClearLines()
   -- Got to override GameTooltip.GetItem(), such that other addons can still use it
   -- to learn which item is displayed. Will be restored after GameTooltip:OnHide() (see above).
   self.GetItem = function(self) return name, link end
-  
+
   -- Refill the tooltip with the stored lines plus the new "per unit" money frame.
   for i = 1, moneyFrameLineNumber-1, 1 do
-  
+
     if rightText[i] then
       self:AddDoubleLine(leftText[i], rightText[i], leftTextR[i], leftTextG[i], leftTextB[i], rightTextR[i], rightTextG[i], rightTextB[i])
     else
@@ -121,14 +123,14 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
       --       Therefore, we have to put wrap=true for all lines in the new tooltip.
       self:AddLine(leftText[i], leftTextR[i], leftTextG[i], leftTextB[i], true)
     end
-  
+
   end
-  
+
   SetTooltipMoney(self, itemSellPrice*stackCount, nil, string_format("%s:", SELL_PRICE))
   SetTooltipMoney(self, itemSellPrice, nil, string_format("%s %s:", SELL_PRICE, AUCTION_PRICE_PER_ITEM))
-  
+
   for i = moneyFrameLineNumber+1, numLines, 1 do
-  
+
     if rightText[i] then
       self:AddDoubleLine(leftText[i], rightText[i], leftTextR[i], leftTextG[i], leftTextB[i], rightTextR[i], rightTextG[i], rightTextB[i])
     else
@@ -136,9 +138,9 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
       --       Therefore, we have to put wrap=true for all lines in the new tooltip.
       self:AddLine(leftText[i], leftTextR[i], leftTextG[i], leftTextB[i], true)
     end
-  
+
   end
-  
+
 end
 )
 
